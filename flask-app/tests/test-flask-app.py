847bb888b6fe0bf074a450aba57e335808fe93da
Mary Goldena Sharon <123451482+MaryGoldSha@users.noreply.github.com>
2023-08-08 18:37:20 +0530

    Update test-flask-app.py

--- flask-app/tests/test-flask-app.py
+++ flask-app/tests/test-flask-app.py
@@ -1,1 +1,17 @@
-#
+import unittest
+from flask import Flask
+from src.app import app
+
+class TestFlaskApp(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_hello_bmw(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode('utf-8'), 'Hello BMW!')
+
+if __name__ == '__main__':
+    unittest.main()
+
